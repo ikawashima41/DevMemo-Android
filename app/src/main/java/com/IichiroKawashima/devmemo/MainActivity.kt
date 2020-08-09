981627897f5e70883273a3e165cb5ddfc4501f54
ikawashima41 <i.kawashima41@gmail.com>
2020-08-10 05:57:46 +0900

    Fix activity

--- app/src/main/java/com/IichiroKawashima/devmemo/MainActivity.kt
+++ app/src/main/java/com/IichiroKawashima/devmemo/MainActivity.kt
@@ -1,43 +1,13 @@
 package com.IichiroKawashima.devmemo
 
-import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import android.widget.Toast
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseAuthWebException
 
 class MainActivity : AppCompatActivity() {
-
-    private  lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        auth = FirebaseAuth.getInstance()
-    }
-
-    override fun onStart() {
-        super.onStart()
-
-        val currentUser = auth.currentUser
-    }
-
-    fun login(email: String, password: String) {
-        auth.signInWithEmailAndPassword(email, password)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    val user = auth.currentUser
-                } else {
-                    Toast.makeText(baseContext, "Authentication failed", Toast.LENGTH_SHORT).show()
-                }
-            }
-    }
-
-    fun logout() {
-        FirebaseAuth.getInstance().signOut()
     }
 }
--- app/src/main/java/com/IichiroKawashima/devmemo/Service/FirebaseAuth.kt
+++ app/src/main/java/com/IichiroKawashima/devmemo/Service/FirebaseAuth.kt
@@ -1,2 +1,17 @@
 package com.IichiroKawashima.devmemo.Service
 
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+
+object FirebaseAuth {
+
+    fun login(email: String, password: String) {
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password).addOnSuccessListener { result ->
+            val user = FirebaseAuth.getInstance().currentUser
+        }
+    }
+
+    fun logout() {
+        FirebaseAuth.getInstance().signOut()
+    }
+}
