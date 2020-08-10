e2087ed8f3048cee9773fdcebefe30f12ed27d2e
ikawashima41 <i.kawashima41@gmail.com>
2020-08-11 08:28:27 +0900

    Install recyclerview

--- app/src/main/java/com/IichiroKawashima/devmemo/HomeActivity.kt
+++ app/src/main/java/com/IichiroKawashima/devmemo/HomeActivity.kt
@@ -1,12 +1,13 @@
 package com.IichiroKawashima.devmemo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(R.layout.activity_list_item)
     }
 }
